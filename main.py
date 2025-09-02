427a514d8e775f3a2d829acf478eeae708d1a560
Ahmed Abdelalem <abdelalem133@gmail.com>
2025-09-02 18:00:06 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")           # load the values from .env in the system
+
+from routes import base
 
 app=FastAPI()
 
-@app.get("/welcome")
-def welcomd():
-    return {
-        "message":"Hello World!"
-    }
+app.include_router(base.base_router)
