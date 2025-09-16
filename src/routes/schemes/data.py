eccfbdb67fa0d97a3607abd600304f5dc4044259
Ahmed Abdelalem <abdelalem133@gmail.com>
2025-09-16 03:44:49 +0300

    enhance file processing logic; add error handling for missing files and update asset retrieval to retrieve one file using file id or to retrieve all files using project id

--- src/routes/schemes/data.py
+++ src/routes/schemes/data.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ProcessRequest(BaseModel):
-    file_id: str
+    file_id: str = None
     chunk_size: Optional[int] = 100   # it will be 100 if the user did not pass it
     overlap_size: Optional[int] = 20  # it will be 20 if the user did not pass it
     do_reset: Optional[int] = 100     # it will be 100 if the user did not pass it
