3db0e3162f242049bc57bb6dde06d566f427e3b0
Ahmed Abdelalem <abdelalem133@gmail.com>
2025-09-30 11:47:56 +0300

    edit embed_text func to make it embed batch by batch

--- src/store/llm/providers/OpenAIProvider.py
+++ src/store/llm/providers/OpenAIProvider.py
@@ -2,6 +2,7 @@ from store.llm.LLMInterface import LLMInterface
 from store.llm.LLMEnums import OpenAIEnums
 from openai import OpenAI
 import logging
+from typing import List, Union
 
 class OpenAIProvider(LLMInterface):
 
@@ -69,12 +70,15 @@ class OpenAIProvider(LLMInterface):
     
         return response.choices[0].message.content
     
-    def embed_text(self, text: str, document_type: str = None):
+    def embed_text(self, text: Union[ str, List[str] ], document_type: str = None):
         
         if not self.client:
             self.logger.error("OpenAI client was not set")
             return None
         
+        if isinstance(text,str):
+            text=[text]
+                
         if not self.embedding_model_id:
             self.logger.error("Embedding model for OpenAI was not set")
             return None
@@ -88,7 +92,7 @@ class OpenAIProvider(LLMInterface):
             self.logger.error("Error while embedding text with OpenAI")
             return None
         
-        return response.data[0].embedding
+        return [ rec.embedding for rec in response.data]
     
     def process_text(self, text: str):
         return text[:self.default_input_max_characters+1].strip()
